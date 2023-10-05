c04003af3e13341e35c4eb5888a116be8c572647
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-10-05 13:22:45 +0530

    Added gitignore

---
+++ Basics/Concept/B-01-TypeCasting.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // \ is called as escape character
+    cout << "\t" << "Hello" << "\n";
+
+
+    // Type casting
+    cout << (char)('f' + 1) << "\n";            // 'f' + 1 gives 103 without type casting
+
+
+
+    char c = '4';
+    // How to convert this 4 to integer 4 
+    cout << c - '0' << endl;
+
+
+    // NOTE : Character must be in single quotes
+
+    // NOTE : String is a Derived Datatype same as other Vector, map, set, etc  built on characters
+
+    // INPUT way -> Ek value krne ke baad aap kitna bhi space leke second value, no issue
+
+    // REMEMBER : In C++, by BODMAS all calculation is done
+
+
+
+    // String input :
+    string s;
+    getline(cin,s);
+
+
+
+    // Float divison: 
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << (float)a/b << "\n";
+    cout << a * 1.0 / b << endl;     // Remember -> a / b * 1.0 will give integer (BODMAS) -> a / ( b * 1.0 )  
+    // Or you can take input variables in FLOAT
+
+    
+
+    return 0;
+
+}
---
+++ Basics/Problems/Problem-01.cpp
@@ -0,0 +1,54 @@
+// Given three numbers A, B, C. Print these numbers in ascending order followed by a blank line and 
+// then the values in the sequence as they were read.
+
+// Input : 3 -2 1
+// Output:
+// -2
+// 1
+// 3
+
+// 3
+// -2
+// 1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int a,b,c;
+    cin >> a >> b >> c;
+    
+    // 1 way :
+    int mx = max(a,max(b,c));
+    int mi = min(a,min(b,c));
+    int t = (a+b+c) - (mx + mi);
+
+    // Without min , max
+    
+    int x = a, y = b , z = c;
+    
+    if(a > b){
+        swap(a,b);
+    }
+
+
+    if(b>c){
+        swap(b,c);
+        if(a > b){
+            swap(a,b);
+        }
+    }
+
+    
+    cout<< mi<<endl;
+    cout<<t<<endl;
+    cout<<mx<<endl;
+    cout<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    
+    return 0;
+    
+}
