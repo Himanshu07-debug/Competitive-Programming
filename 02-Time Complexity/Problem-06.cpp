2e6f63a3bc6d1726292ab4f3f1168cc3dfa3a1fe
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-10-12 18:33:08 +0530

    Added Problems in Time Complexity folder

--- 02-Time Complexity/Problem-06.cpp
+++ 02-Time Complexity/Problem-06.cpp
@@ -37,8 +37,31 @@ int main() {
 
 // PROBLEM SETTER NEVER MAKE A QUESTION THAT IS NOT DOABLE
 
+// ONE MORE EXAMPLE FOR BETTER UNDERSTANDING:
 
-//  NOTE : If it was asked that it want Unique Numbers, then -> 
+// Let's call a positive integer composite if it has at least one divisor other than 1 and itself. For example: 
+// the following numbers are composite: 1024, 4, 6, 9;
+// the following numbers are not composite: 13, 1, 2, 3, 37
+// You are given a positive integer n. Find two composite integers a,b such that a − b = n
+// It can be proven that solution always exists.
+
+// Constraint : 1 <= n <= 10^7  , 2 <= a,b <= 10^9
+
+// Input n=1  ===> 9 8  [ a=9 & b=8 ]
+// Input n = 512  ===> 4608 4096
+
+// Composite numbers are NON-PRIME no.s
+
+// Constraints are TIGHT to find a and b ,Observe and  try to reduce the equation a - b = n
+
+// LOGIC : Any no. multiplied with a Composite number is a Composite Number
+
+// REDUCING THE EQUATION -> 9*n - 8*n = n  [ 9*n, 8*n are Composite Numbers becz 9,8 are both composite number ]
+
+// Other numbers are also Possible, but try to figure out the pairs of composite numbers i.e satisfying here
+
+
+//  NOTE : In the first Example,  If it was asked that it want Unique Numbers, then -> 
 
 // Run a for loop and find out that till which fibonacci no.s the sum can be upto 10^9, also use dp array to store 
 // the Previous sets
