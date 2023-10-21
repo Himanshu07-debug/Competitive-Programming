5778dfe648ecd6f0761988ff1039a414eeb81c57
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-10-21 23:59:47 +0530

    commited

---
+++ 04-Number Theory/Problems/Problem-01.cpp
@@ -0,0 +1,12 @@
+// P % a = P % b
+// 2 ≤ a < b ≤ P    P --> Prime
+
+// Constraint : 5 ≤ P ≤ 10^9  ,  2 ≤ a < b ≤ P
+
+// LOGIC : Simple question becz constraint are helping , a can be 2 , and we remainder of 2 with any number is 0 or 1, so we will take a = 2
+
+// P is Prime , so it will take only odd values, becz 2 is the only prime no. that is even
+
+
+
+
