a7ee65e9c29f98249b67dbfff64029ce264857d7
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-10-24 13:31:52 +0530

    Updated Short.txt

--- 04-Number Theory/Problems/Problem-01.cpp
+++ 04-Number Theory/Problems/Problem-01.cpp
@@ -5,7 +5,7 @@
 
 // LOGIC : Simple question becz constraint are helping , a can be 2 , and we remainder of 2 with any number is 0 or 1, so we will take a = 2
 
-// P is Prime , so it will take only odd values, becz 2 is the only prime no. that is even
+// P is Prime , so it will take only odd values, becz 2 is the only prime no. that is even and P starts from 5
 
 #include<bits/stdc++.h>
 using namespace std;
