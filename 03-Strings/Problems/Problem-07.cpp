0da2c542eca84b7b98ee058385e39ec7cb3de0f9
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-11-24 20:27:25 +0530

    Updated

--- 03-Strings/Problems/Problem-07.cpp
+++ 03-Strings/Problems/Problem-07.cpp
@@ -6,3 +6,43 @@
 
 // LOGIC -------------------->>
 
+// There are 2 Approaches ------>>
+
+// Approach 1 ===========================>
+// We know that ki border n/2 se jyada nhi honga becz same substring muje right me bhi chahiye .. n/2 se big kiya to right wali less that n/2 hongi
+// To hum first n/2 characters ke hash aaise nikalenge --> hash[i] = hash[i-1] + (s[i] * p )  ,, p = p * 31
+// Last ke n/2 hum reverse traverse krnge and unka hash --> hash[i] = hash[i+1]*p + s[i]  ,, p = p * 31
+
+// Dry run krke dekho hash same aa rhe hai checking ke liye
+
+
+
+// APPROACH 2 ===========================>>>
+
+// (0, i) ka corresponding (n-1-i , n-1) range ke hash value se check krnge
+// Suffix Prefix nhi rhnga, Same like Prefix, Ek hi Prefix array rhnga
+
+
+// NOTE => Hum Second Approach use krnge .. First one is also easy ..
+// In Both of them, For Double hashing, use 2 hash array -> hash1  & hash2
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+const long long MOD = 1e9 + 7;
+const long long INF = LLONG_MAX >> 1; 
+const long long NINF = LLONG_MIN;
+
+
+int main(){
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    // Your code here
+    
+    
+    
+    return 0;
+}
